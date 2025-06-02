fdb2d3afa93886bc1c79ce89800f8bbf6698d68e
vothanhdo2602@gmail.com <vothanhdo2602@gmail.com>
2025-06-02 17:21:49 +0700

    add type for constant

--- constant/db.go
+++ constant/db.go
@@ -1,25 +1,32 @@
 package constant
 
+// DBType defines the database type
+type DBType string
+
 const (
-	DBPostgres  = "postgres"
-	DBMysql     = "mysql"
-	DBMOracle   = "oracle"
-	DBSQLServer = "sqlserver"
+	DBPostgres  DBType = "postgres"
+	DBMysql     DBType = "mysql"
+	DBMOracle   DBType = "oracle"
+	DBSQLServer DBType = "sqlserver"
 )
 
 // RelationType defines the type of relationship
-const (
-	HasOne        = "has-one"
-	BelongsTo     = "belongs-to"
-	HasMany       = "has-many"
-	HasManyToMany = "has-many-to-many"
-)
+type RelationType string
 
 const (
-	ColumnTypeString    = "string" // "text", "varchar", "char"
-	ColumnTypeTimestamp = "timestamp"
-	ColumnTypeInteger   = "int"
-	ColumnTypeFloat     = "float"
-	ColumnTypeBoolean   = "boolean"
-	ColumnTypeJSON      = "json"
+	HasOne        RelationType = "has-one"
+	BelongsTo     RelationType = "belongs-to"
+	HasMany       RelationType = "has-many"
+	HasManyToMany RelationType = "has-many-to-many"
+)
+
+type ColumnType string
+
+const (
+	ColumnTypeString    ColumnType = "string" // "text", "varchar", "char"
+	ColumnTypeTimestamp ColumnType = "timestamp"
+	ColumnTypeInteger   ColumnType = "int"
+	ColumnTypeFloat     ColumnType = "float"
+	ColumnTypeBoolean   ColumnType = "boolean"
+	ColumnTypeJSON      ColumnType = "json"
 )
